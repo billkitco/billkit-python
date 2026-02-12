85ed8ee2ccf844e34111c3363ce2b094e880d69a
billkitco <billkitco@gmail.com>
2026-02-12 05:15:18 +0000

    docs: add docstring to Reports class

--- src/billkit/api/reports/__init__.py
+++ src/billkit/api/reports/__init__.py
@@ -7,10 +7,19 @@ class Reports:
         self.requester = requester
 
     def get_revenue(self, currency: str | None = None):
+        """
+        Fetch the current user's revenue for a given currency. If no currency is passed as a param, 
+        Returns:
+            RevenueReportResponse: The authenticated user's revenue report.
+
+        Example:
+            report = client.reports.get_revenue("GBP")
+            print(report.summary)
+        """
         endpoint: str = "reports/revenue"
         if (
             currency is None
-        ):  # When no currency is added, behaviour defaults to using default currency of the authenticated account
+        ):
             response_data = self.requester("GET", endpoint)
         else:
             response_data = self.requester("GET", f"{endpoint}?currency={currency}")
