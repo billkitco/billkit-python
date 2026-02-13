5a51d8c234ee8e2bf77d0b516af83723bf891396
billkitco <billkitco@gmail.com>
2026-02-13 23:20:50 +0000

    feat: add delete method to Quotes class

--- src/billkit/api/quotes/__init__.py
+++ src/billkit/api/quotes/__init__.py
@@ -1,4 +1,13 @@
+from collections.abc import Callable
+
+from ...models.quotes import QuoteDeleteResponse
 from .._base import _BaseDocuments
 
 
-# class Quotes(_BaseDocuments): ...
+class Quotes(_BaseDocuments):
+    def __init__(self, requester: Callable):
+        super().__init__(requester)
+
+    def delete(self, file_id: str):
+        response_data = self._requester("DELETE", f"quotes?file_id={file_id}")
+        return QuoteDeleteResponse(**response_data)
