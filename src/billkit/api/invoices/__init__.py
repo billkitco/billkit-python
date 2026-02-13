52df432777cb13fbbe699aa4269e1539efa6dc1a
billkitco <billkitco@gmail.com>
2026-02-13 23:08:29 +0000

    chore: remove print statement

--- src/billkit/api/invoices/__init__.py
+++ src/billkit/api/invoices/__init__.py
@@ -25,5 +25,4 @@ class Invoices(_BaseDocuments):
 
     def delete(self, file_id: str):
         response_data = self._requester("DELETE", f"invoices?file_id={file_id}")
-        print(response_data)
         return response_data
